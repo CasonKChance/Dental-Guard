69aa00f8c001c75f2515405a6c8fccbc5b78f8b1
Brandon Michaels <brandonmichaels@lawn-143-215-102-170.lawn.gatech.edu>
2024-02-09 23:12:14 -0500

    Add basic outline and images to front end

--- dentalguard-frontend/src/App.js
+++ dentalguard-frontend/src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    document.title = 'DentalGuard';
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app-container">
+      {/* Banner Section */}
+      <div className="banner-container">
+        <h1>Dental Guard</h1>
+        <p width="20%">AI Oral Hygiene Detector for low-income areas without connection to healthcare facilities.</p>
+        <button>Demo Now</button>
+      </div>
+
+      {/* Paragraph with Two Images */}
+      <div className="image-paragraph-container">
+        <h1>About Us</h1>
+        <br></br>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel
+          purus eget justo egestas eleifend vel id neque.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="image-wrapper">
+          <img src="aboutImage1.jpg" alt="Image 1" />
+          <img src="aboutImage2.jpg" alt="Image 2" />
+        </div>
+      </div>
+
+      {/* Section with Image Upload Button */}
+      <div className="image-upload-section">
+        <h2>Upload Dental Image</h2>
+        <div className="upload-container">
+          <input type="file" id="imageUpload" />
+          <label htmlFor="imageUpload">Upload Image</label>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
